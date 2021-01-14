fa2339fae852ef4d4f40e652e5c09d6536d68a17
luongdoan277 <luongdoan277@gmail.com>
2021-01-14 23:42:26 +0700

    update

--- AdminPage/Startup.cs
+++ AdminPage/Startup.cs
@@ -50,7 +50,7 @@ namespace AdminPage
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Dashboard}/{action=Index}/{id?}");
             });
         }
     }
