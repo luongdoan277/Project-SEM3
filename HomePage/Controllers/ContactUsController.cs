ea56ac2b366237f73d2532e21f1263ebdbcfbb62
Khanh Vuong <khanhvuong987@gmail.com>
2021-02-27 11:56:55 +0700

    up

--- HomePage/Controllers/ContactUsController.cs
+++ HomePage/Controllers/ContactUsController.cs
@@ -1,13 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HomePage.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using HomePage.Models.ViewModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace HomePage.Controllers
 {
     public class ContactUsController : Controller
     {
+        private readonly StoreDbContext context;
+
+        public ContactUsController(StoreDbContext _context)
+        {
+            context = _context;
+        }
+
         public IActionResult Index()
         {
             return View();
